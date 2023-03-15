6b4b9822fad55624ffdfafb7aa5a825be54315b4
Felipe Falco <falco.felipe27@gmail.com>
2023-03-15 16:16:26 +1300

    Test

--- server.js
+++ server.js
@@ -35,4 +35,6 @@ if (process.env.NODE_ENV === 'production') {
 
 const PORT = process.env.PORT || 5000; //To account for deployment and development
 
+console.log(PORT);
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
